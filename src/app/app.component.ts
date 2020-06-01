63b4bbb91c5fa4caa125eabc43b467a7b51be4ed
unknown <realffootball@gmail.com>
2020-06-01 03:52:33 +0300

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 's-viewer';
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.http.get(environment.apiUrl).subscribe(res => console.log(res));
+  }
+
 }
